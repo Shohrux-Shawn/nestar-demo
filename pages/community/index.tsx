27537a090d7eed19dfc872cb215d67afbf38ae43
SHAWN <shakh3973@gmail.com>
2025-10-22 17:45:49 +0900

    feat: organize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,18 @@
+
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
+import { useState } from "react";
+
+const Community: NextPage = () => {
+    const [title, setTitle] = useState<string>("hello");
+    return( 
+        <div>
+            COMMUNITY{" "}
+            <button onClick={() => alert("Hello Magnus")} style={{ margin: "15px"}}>
+                PressMe
+            </button>
+        </div>
+    );    
+};
+
+export default withLayoutBasic(Community);
